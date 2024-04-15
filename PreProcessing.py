c8a143df25dbdada982228c8daef04f483d653dc
Mingi Hong <mingi.hong@SoDakLabs.com>
2024-04-15 21:41:07 +0900

    html 자동 저장

--- PreProcessing.py
+++ PreProcessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import mpld3
+import folium
+from geopy.distance import geodesic
 
 data = pd.read_csv('data/서울교통공사_역별 일별 시간대별 승하차인원 정보_23.11_24.01.csv', encoding='cp949')
 
@@ -32,4 +33,36 @@ plt.title("역별 유동인구")
 plt.xlabel("역명")
 plt.ylabel("유동인구")
 plt.savefig("./SeoulCafeLocationTopFive/src/main/webapp/img/stationMovePopulation.png")
-plt.show()
+
+seoulStationDF = pd.read_csv("./data/seoulStation.csv")
+
+seoulStationCafe = seoulStationDF[seoulStationDF["상권업종소분류명"] == "카페"]
+
+# 지도의 중심을 설정합니다
+map = folium.Map(location=[37.5558, 126.9723], zoom_start=17)
+
+# 원을 그릴 위치의 위도와 경도를 설정합니다
+circle_lat_lon = [37.5558, 126.9723]
+
+# 원을 추가합니다
+folium.Circle(
+    location=circle_lat_lon,
+    radius=200,  # 원의 반지름 (미터 단위)
+    color='crimson',
+    fill=True,
+    fill_color='crimson'
+).add_to(map)
+
+# 중복된 위치에 마커를 찍기 위한 작은 변화량
+delta = 0.00001
+count = 0
+mapCopied = map
+for index, row in seoulStationCafe.iterrows():
+    location = (row['위도']+ delta * count, row['경도']+ delta * count)
+    print(location)
+    count += 1
+    folium.Marker(location = location,
+                  popup=row["상호명"],
+              tooltip=row["상호명"]).add_to(mapCopied)
+
+mapCopied.save("./SeoulCafeLocationTopFive/src/main/webapp/seoulStationCafe.html")
